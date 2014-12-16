cd11aaa096065fa577fe17968be6178d28126b07
ryzy <marcin@ryzycki.com>
2014-12-16 23:45:56 +0000

    Init commit

---
+++ Classes/M12/SlickCarousel/Package.php
@@ -0,0 +1,30 @@
+<?php
+namespace M12\SlickCarousel;
+
+/*                                                                        *
+ * This script belongs to the "M12.SlickCarousel" package.                   *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Package\Package as BasePackage;
+
+/**
+ * The M12.SlickCarousel Package
+ */
+class Package extends BasePackage {
+
+	/**
+	 * Invokes custom PHP code directly after the package manager has been initialized.
+	 *
+	 * @param \TYPO3\Flow\Core\Bootstrap $bootstrap The current bootstrap
+	 * @return void
+	 */
+	public function boot(\TYPO3\Flow\Core\Bootstrap $bootstrap) {
+//		$dispatcher = $bootstrap->getSignalSlotDispatcher();
+	}
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# M12.SlickCarousel - Slick carousel inside TYPO3 Neos
+
+This [TYPO3 Neos](http://neos.typo3.org/) implementation of [Slick Carousel](http://kenwheeler.github.io/slick/) as a new node type.
+
+## Usage
+
+* Add it to your composer.json:  
+```
+        "require": {
+            "...": "*",
+            "m12/neos-nodetype-slickcarousel": "dev-master"
+        },
+```
+* Add required CSS and JS to your page template. Follow the instructions here: http://kenwheeler.github.io/slick/ .  
+  You can also look at [M12.FoundationSite](https://github.com/million12/M12.FoundationSite) site package, where this plugin is used, together with [M12.Foundation](https://github.com/million12/M12.Foundation) implementing all Zurb Foundation components.
+
+## Author(s)
+
+* Marcin Ryzycki marcin@m12.io  
